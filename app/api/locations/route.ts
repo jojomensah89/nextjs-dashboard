dd6d1be6280e6add693acaa2676d6ca99e457e42
ebenezermensah <ebenezer.mensah@amalitech.org>
2024-01-16 15:12:19 +0000

    added api locations route

---
+++ app/api/locations/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+import { locations } from "@/app/lib/locations";
+export async function GET(req: Request, res: Response) {
+  return NextResponse.json(locations, {
+    status: 200,
+  });
+}
