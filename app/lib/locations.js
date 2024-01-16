15eb6689ecb005e40e5ee17b2d7a03708ef62855
Jojo Mensah <jojomensah89@gmail.com>
2024-01-16 15:32:43 +0000

    Update locations.js

--- app/lib/locations.js
+++ app/lib/locations.js
@@ -1,3 +1,6 @@
+const baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+
 const locations =
     [
         {
@@ -5,7 +8,7 @@ const locations =
             "name": "Acme Fresh Start Housing",
             "city": "Chicago",
             "state": "IL",
-            "photo": "${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
+            "photo": "${baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
             "availableUnits": 4,
             "wifi": true,
             "laundry": true
@@ -15,7 +18,7 @@ const locations =
             "name": "A113 Transitional Housing",
             "city": "Santa Monica",
             "state": "CA",
-            "photo": "${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg",
+            "photo": "${baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg",
             "availableUnits": 0,
             "wifi": false,
             "laundry": true
@@ -25,7 +28,7 @@ const locations =
             "name": "Warm Beds Housing Support",
             "city": "Juneau",
             "state": "AK",
-            "photo": "${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
+            "photo": "${baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
             "availableUnits": 1,
             "wifi": false,
             "laundry": false
@@ -35,7 +38,7 @@ const locations =
             "name": "Homesteady Housing",
             "city": "Chicago",
             "state": "IL",
-            "photo": "${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
+            "photo": "${baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
             "availableUnits": 1,
             "wifi": true,
             "laundry": false
@@ -45,7 +48,7 @@ const locations =
             "name": "Happy Homes Group",
             "city": "Gary",
             "state": "IN",
-            "photo": "${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
+            "photo": "${baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
             "availableUnits": 1,
             "wifi": true,
             "laundry": false
@@ -55,7 +58,7 @@ const locations =
             "name": "Hopeful Apartment Group",
             "city": "Oakland",
             "state": "CA",
-            "photo": "${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
+            "photo": "${baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
             "availableUnits": 2,
             "wifi": true,
             "laundry": true
@@ -65,7 +68,7 @@ const locations =
             "name": "Seriously Safe Towns",
             "city": "Oakland",
             "state": "CA",
-            "photo": "${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
+            "photo": "${baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
             "availableUnits": 5,
             "wifi": true,
             "laundry": true
@@ -75,7 +78,7 @@ const locations =
             "name": "Hopeful Housing Solutions",
             "city": "Oakland",
             "state": "CA",
-            "photo": "${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg",
+            "photo": "${baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg",
             "availableUnits": 2,
             "wifi": true,
             "laundry": true
@@ -85,7 +88,7 @@ const locations =
             "name": "Seriously Safe Towns",
             "city": "Oakland",
             "state": "CA",
-            "photo": "${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg",
+            "photo": "${baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg",
             "availableUnits": 10,
             "wifi": false,
             "laundry": false
@@ -95,7 +98,7 @@ const locations =
             "name": "Capital Safe Towns",
             "city": "Portland",
             "state": "OR",
-            "photo": "${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg",
+            "photo": "${baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg",
             "availableUnits": 6,
             "wifi": true,
             "laundry": true
