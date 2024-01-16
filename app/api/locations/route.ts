a2e882018d77011b6f94a5f6b3a23445881ed7bb
Jojo Mensah <jojomensah89@gmail.com>
2024-01-16 16:02:14 +0000

    Update route.ts

--- app/api/locations/route.ts
+++ app/api/locations/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { locations } from "@/app/lib/locations";
 export async function GET(req: Request, res: Response,  { params }: { params: { id: string } }
 ) {
-   const id = Number(params.slug); 
+   const id = Number(params.id); 
   if(id){
      return NextResponse.json(locations[id], {
     status: 200,
